b2c967a2c660fecd1e4ad517834358d82a211187
BabaAfrica <boubakeradem47@gmail.com>
2025-01-11 02:22:50 +0100

    changes

--- School_System/ui/__init__.py
+++ School_System/ui/__init__.py
@@ -1,3 +1,14 @@
 import os
 LOGIN = os.path.join(os.path.dirname(__file__), "login.ui")
 
+LOGIN = os.path.join(os.path.dirname(__file__), "AddStudentDialog.ui")
+
+LOGIN = os.path.join(os.path.dirname(__file__), "AddTeacherDialog.ui")
+
+LOGIN = os.path.join(os.path.dirname(__file__), "AddSubjectDialog.ui")
+
+LOGIN = os.path.join(os.path.dirname(__file__), "CreateAccountDialog.ui")
+
+LOGIN = os.path.join(os.path.dirname(__file__), "indexSU.ui")
+
+LOGIN = os.path.join(os.path.dirname(__file__), "AddClassDialog.ui")
