c76672d1b7e8508188699e4d0f3d098a5941ae9e
BabaAfrica <boubakeradem47@gmail.com>
2025-01-17 11:03:39 +0100

    changes

--- School_System/windows/AddStudentDialog.py
+++ School_System/windows/AddStudentDialog.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QDialog, QMessageBox
+from PyQt6.QtWidgets import QDialog, QMessageBox, QFileDialog
 from PyQt6 import uic
 import School_System.helpers.db_utils as database
 from School_System.ui import ADD_STUDENT_DIALOG
@@ -13,8 +13,11 @@ class AddStudentDialog(QDialog):
         self.setWindowTitle("Add Student")
 
         self.add_student_button.clicked.connect(self.add_student)
+        self.add_pic.clicked.connect(self.open_image_dialog)
 
         self.classes_dropdown.addItem("N/A")
+        self.image_bin = None
+
 
         classes = database.get_classes_ids()
         for class_id, class_name in classes:
@@ -29,6 +32,39 @@ class AddStudentDialog(QDialog):
         self.phone_field.clear()
         self.email_field.clear()
         self.address_field.clear()
+
+
+
+    def open_image_dialog(self):
+        # Open the file dialog
+        image_path, _ = QFileDialog.getOpenFileName(
+            self,
+            "Open Image",
+            "",  # Starting directory
+            "Images (*.png *.jpg *.jpeg *.bmp *.gif)"  # File filter
+        )
+
+        # Validate the file type
+        if image_path and image_path.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
+            with open(image_path, "rb") as file:
+                binary_data = file.read()
+            self.image_bin = binary_data
+
+
+        else:
+            self.image_bin = None
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -48,9 +84,37 @@ class AddStudentDialog(QDialog):
         stdclass = self.classes_dropdown.currentText()
         class_id = self.classes_dropdown.currentData()
 
+
         if not name or not last_name or not phone or not email or not address:
             QMessageBox.warning(self, "Input Error", "Please fill in all required fields.")
             return
+
+
+        if self.image_bin:
+            if stdclass == "N/A":
+                evaluate = database.add_student(full_name, phone, email, gender, birth_date, address)
+                if evaluate == "Student added successfully":
+                    QMessageBox.information(self, "info", f"{evaluate}")
+                    self.clear_fields()
+                    self.index_instance.load_students_to_table()
+                    self.index_instance.update_students_count()
+                    self.index_instance.refresh_setup_activity_log__table()
+                    return
+                else:
+                    QMessageBox.warning(self, "Error", f"{evaluate}")
+                    return
+
+            evaluate = database.add_student(full_name, phone, email, gender, birth_date, address, class_id)
+            if evaluate == "Student added successfully":
+                QMessageBox.information(self, "info", f"{evaluate}")
+                self.clear_fields()
+                self.index_instance.load_students_to_table()
+                self.index_instance.update_students_count()
+                self.index_instance.refresh_setup_activity_log__table()
+                return
+            else:
+                QMessageBox.warning(self, "Error", f"{evaluate}")
+
 
 
         if stdclass == "N/A":
